b2b082fb081a86f0dc2720ec15c0adc71fe53fde
Amparo Soeli Betancourt Tarifa <74368835+amparobetancourt@users.noreply.github.com>
2025-04-14 20:33:32 +0200

    Update do_build.sh

--- TASK1_baseline/do_build.sh
+++ TASK1_baseline/do_build.sh
@@ -4,7 +4,7 @@
 set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DOCKER_IMAGE_TAG="example-algorithm-open-development-phase"
+DOCKER_IMAGE_TAG="PANTHER-task1-baseline"
 
 
 # Check if an argument is provided
